336647852ddcdde7f37567e8463c22e036e250be
Aaron Sleeper <asleeper@fairwaytech.com>
2018-04-06 15:54:18 -0700

    background image done finally

--- app/scripts/scripts.js
+++ app/scripts/scripts.js
@@ -1,17 +1,36 @@
-var angleOfRotation = 45;
+/* ------------------------------
+
+Equations being used are from the link below
+https://math.stackexchange.com/questions/592701/calculating-dimensions-of-rotated-rectangle-for-it-to-to-mask-original?rq=1
+
+eventually this will be used to cover the entire <body>
+
+for now i've sized it down by 1/2 so i can see what i'm doing
+
+------------------------------ */
+
+var angleOfRotation = .785;
 
 function sizeBackground() {
+    var rot = Math.round(180.0 * angleOfRotation / 3.1416);
+
     // get the viewport dimensions
     var vw = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
     var vh = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+
     // calculate the size of our rectangle
     var rw = (vh*Math.sin(angleOfRotation)) + (vw*Math.cos(angleOfRotation));
     var rh = (vw*Math.sin(angleOfRotation)) + (vh*Math.cos(angleOfRotation));
-    console.log('vw: ' + vw, 'vh: ' + vh, 'rw: ' + Math.round(rw), 'rh: ' + Math.round(rh));
+    console.log('vw: ' + vw, 'vh: ' + vh, 'rw: ' + Math.round(rw), 'rh: ' + Math.round(rh), 'rot: ' + rot);
 
     // set the values to custom props on the body
+    document.body.style.setProperty('--vw', vw + 'px');
+    document.body.style.setProperty('--vh', vh + 'px');
     document.body.style.setProperty('--rw', rw + 'px');
     document.body.style.setProperty('--rh', rh + 'px');
+    document.body.style.setProperty('--rot', rot + 'deg');
+    document.body.style.setProperty('--rtop', Math.round((vh - rh)/2) + 'px');
+    document.body.style.setProperty('--rleft', Math.round((vw - rw)/2) + 'px');
 }
 
 // Run all the functions
